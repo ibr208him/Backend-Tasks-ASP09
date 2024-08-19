2cbf0716a92fe5b642790fdba8baa8a3ab548962
Ibrahim Maali <maaliibrahim2022@gmail.com>
2024-08-20 02:54:07 +0300

    add MVC__task1

---
+++ MVC__Task1__Ibrahim Maali/learning1/learning1/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace learning1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetOne()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
